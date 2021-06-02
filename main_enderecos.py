2ede0bade65f4eb1dd1544b946975c8b6807b8d8
Alexandre Bertulino <alexandre.junior@inmetrics.com.br>
2021-06-02 10:40:14 -0300

    Alterando

--- main_enderecos.py
+++ main_enderecos.py
@@ -136,4 +136,4 @@ def not_found(error=None):
     return response
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5001)
+    app.run(debug=True, host="0.0.0.0", port=80)
